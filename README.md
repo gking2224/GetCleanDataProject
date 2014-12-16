2c7d1e3e32bde9a23fd8c8ed615694c09af79795
gking2224 <gking2224@gmail.com>
2014-12-16 22:22:21 +0000

    Minor updates, typo corrections etc.

--- README.md
+++ README.md
@@ -1,21 +1,19 @@
----
-title: "README"
----
+README
+======
 
 Introduction
 ------------
-This is the README guide of my Course Project submission for the John Hopkins Bloomberg School of Public Health course Getting And Cleaning Data, hosted on [Coursera](https://www.coursera.org).
+This is the README guide of my Course Project submission for the [John Hopkins Bloomberg School of Public Health](https://www.coursera.org/jhu) course [Getting And Cleaning Data](https://www.coursera.org/course/getdata), hosted on [Coursera](https://www.coursera.org).
 
 This file describes the function of the script `run_analysis.R`
 
-
-The purpose of this script is to create a file (`subject.activity.means.txt`) summarising the combined test and training datasets for all 30 subjects in the [Human Activity Recognition Using Smartphones Data Set](http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones)
+The purpose of this script is to create a file (`subject.activity.variable.means.txt`) summarising the combined test and training datasets for all mean and standard deviation variables recorded by 30 subjects in the [Human Activity Recognition Using Smartphones Data Set](http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones) project.
 
 The source dataset consists of 561 different measurements recorded using the accelerometer and gyroscope of a Samsung Galaxy S II smartphone device strapped to the subject's waist. Multiple observations of the 561 variables output by the device were taken over a timeframe, during which the subject was taking part in one of six different activities (walking, walking upstairs, walking downstairs, sitting, standing, laying). Measurements were taken for 30 different subjects, who were randomly divided into two groups, split 70% and 30% between the training and test groups respectively.
 
 As such, the measurement data consists of many thousands of observations that can be grouped by dataset (training/test), subject (1-30), activity (1-6), with between 36 and 95 observations of the 561 measurement (features) for each combination of these factors.
 
-The file created is an average of a subset of the 561 variables, grouped by subject and activity. The variables selected for sumarising in this way are only those that provide the 'mean' and 'standard deviation (std)' of all the available measurements.
+The file created contains the mean value of 66 of the total 561 measures, grouped by subject and activity.
 
 Overview of source files used
 -----------------------------
@@ -50,8 +48,8 @@ As such, 66 columns in total were selected from the raw dataset, resulting from 
 
 The selected feature names were normalised in the dataset by removing any punctuation characters (i.e. brackets and hyphens) and converting to lower case. In this way, `tBodyAcc-mean()-X` becomes `tbodyaccmeanx`
 
-Data processing
----------------
+Data processing steps
+---------------------
 The data processing steps taken are:
 
 1. read activities and feature names meta-data  
@@ -72,7 +70,6 @@ _Use `rbind` to concatenate the two datasets. The dataset is enriched with the a
 _Full dataset is now turned from short and wide into long and skinny using `melt` function.`
 9. grouping by subject and activityname, create dataset of the means of each measure  
 _`dcast` is used to create a short and wide dataset of measure means, grouped by activityname and subject. Finally, this data is then re-melted to a tall, skinny dataset before writing to a file, for no other reason that in makes writing the codebook a bit easier!_
-
 
 These steps are correspondingly marked in the script file.
 
--- run_analysis.R
+++ run_analysis.R
@@ -17,11 +17,11 @@ y.test.file <- paste(
     dest.dir, unzipped.dir, "test", "y_test.txt", sep="/")
 subject.test.file <- paste(
     dest.dir, unzipped.dir, "test", "subject_test.txt", sep="/")
-
 activities.file <- paste(
     dest.dir, unzipped.dir, "activity_labels.txt", sep="/")
 features.file <- paste(
     dest.dir, unzipped.dir, "features.txt", sep="/")
+averages.file <- "subject.activity.variable.means.txt"
 
 # download and unzip file
 if (!file.exists(dest.dir)) dir.create(dest.dir)
@@ -30,53 +30,65 @@ if (!file.exists(dest.file)) download.file(
     method="curl")
 if (!file.exists(unzipped.dir)) unzip(dest.file, exdir = dest.dir)
 
-# read activities
+# 1. read activities and feature names meta-data
 activities <- read.table(
     file = activities.file, col.names = c("activityid", "activityname"))
-# read feature names
 features <- read.table(file = features.file)
-cols <- which(regexpr("-mean\\(\\)|-std\\(\\)", features$V2) != -1)
-colNames <- tolower(gsub("-","",sub("\\(\\)","",features[cols,"V2"])))
+
+# 2.  use regular expression to decide column indices and names
+colIndices <- which(regexpr("-mean\\(\\)|-std\\(\\)", features$V2) != -1)
+colNames <- tolower(gsub("-","",sub("\\(\\)","",features[colIndices,"V2"])))
 rm(features)
 
 dataset.levels <- c("train", "test")
 
-# load train dataset
+# 3. load train datasets (measures, activity ids and subject ids)
 X.train.full <- read.table(file = X.train.file)
-X.train <- X.train.full[,cols]
+X.train <- X.train.full[,colIndices]
 names(X.train) <- colNames
 y.train <- read.table(file = y.train.file, col.names = "activityid")
 subject.train <- read.table(file = subject.train.file, col.names="subject")
 
-# combine train files
-train.full <- cbind(
+# 4. combine train datasets using cbind
+train.cmb <- cbind(
     subject.train, y.train, X.train, factor("train", levels = dataset.levels))
-names(train.full) <- c(
+names(train.cmb) <- c(
     names(subject.train), names(y.train), names(X.train), "dataset")
 rm(X.train.full, X.train, y.train, subject.train)
 
-# load test dataset
+# 5. load test datasets (measures, activity ids and subject ids)
 X.test.full <- read.table(file = X.test.file)
-X.test <- X.test.full[,cols]
+X.test <- X.test.full[,colIndices]
 names(X.test) <- colNames
 y.test <- read.table(file = y.test.file, col.names = "activityid")
 subject.test <- read.table(file = subject.test.file, col.names="subject")
 
-# combine test files
-test.full <- cbind(
+# 6. combine test datasets using cbind
+test.cmb <- cbind(
     subject.test, y.test, X.test, factor("test", levels = dataset.levels))
-names(test.full) <- c(
+names(test.cmb) <- c(
     names(subject.test), names(y.test), names(X.test), "dataset")
 rm(X.test.full, X.test, y.test, subject.test)
 
-all.data <- rbind(train.full, test.full)
+# 7. combine test and training datasets and then merge in the activity name
+#    using activityid as key
+all.data <- rbind(train.cmb, test.cmb)
 all.data <- merge(all.data, activities, by = "activityid")
-rm(test.full, train.full, activities)
+rm(test.cmb, train.cmb, activities)
 
 library(reshape2)
+# 8. reshape the data with the 66 measures melted into two columns: variable
+#    and value
 data.melted <- melt(
     all.data, variable.name = "variable", measure.vars = 3:68, id.vars=c(2,70))
 rm(all.data)
 
-subject.activity.means <- dcast(data.melted, subject + activityname ~ variable, mean)
-write.table(subject.activity.means, file="subject.activity.means.txt", row.name=FALSE)
+# 9. grouping by subject and activityname, create final dataset of the means
+#    of each mean and std measure
+subject.activity.means <- dcast(
+    data.melted, subject + activityname ~ variable, mean)
+melted.means <- melt(
+    subject.activity.means, variable.name = "variable",
+    value.name = "mean",
+    measure.vars = 3:68, id.vars = 1:2)
+write.table(melted.means, file = averages.file, row.name=FALSE)
